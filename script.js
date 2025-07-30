18a0102054adb70f35f81ba51f6e8ef43d147d15
Diveheart <kartik.garg3644@gmail.com>
2025-07-31 02:28:23 +0530

    second commit

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const darkModeBtn = document.getElementById('dark-mode-btn');
 
 darkModeBtn.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark');
+
+    if (document.documentElement.classList.contains('dark')) {
+        darkModeBtn.textContent = '💧 Toggle Light Mode';
+    } else {
+        darkModeBtn.textContent = '🌙 Toggle Dark Mode';
+    }
 });
